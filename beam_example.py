60341cc49372a89dff92b24eab55e9879ab8700b
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-05-05 17:59:58 +0300

    build first version of it

--- beam_example.py
+++ beam_example.py
@@ -1,24 +1,40 @@
 import apache_beam as beam
 from apache_beam.io.gcp.internal.clients import bigquery
-from config import file_current
+from apache_beam.options.pipeline_options import PipelineOptions
+from config import (
+    schema, 
+    dataset,
+    table_id,
+    project_id,
+    file_current,
+    temp_location,
+    staging_location
+)
 
 
-with beam.Pipeline() as pipeline:
+pipeline_options = PipelineOptions(
+    runner='DataflowRunner',
+    project=project_id,
+    temp_location=temp_location,
+    staging_location=staging_location,
+    region='us-central1'
+)
+
+with beam.Pipeline(options=pipeline_options) as pipeline:
     values = (
         pipeline
         | beam.io.ReadFromText(file_current, skip_header_lines=True)
         | beam.Map(lambda x: x.split(','))
         | beam.Filter(lambda x: int(x[1]) > 70)
-        | beam.Map(print)
     )
 
-    # values | beam.io.gcp.bigquery.WriteToBigQuery(
-    #     bigquery.TableReference(
-    #         projectId='',
-    #         datasetId='',
-    #         tableId=''
-    #     ),
-    #     schema='',
-    #     write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
-    #     create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED
-    # )
+    values | beam.io.WriteToBigQuery(
+        bigquery.TableReference(
+            projectId=project_id,
+            datasetId=dataset,
+            tableId=table_id
+        ),
+        schema=schema,
+        create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+        write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+    )
--- config.py
+++ config.py
@@ -1,7 +1,26 @@
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 folder_current = os.getcwd()
 folder_csv = 'csv'
 file_csv = 'df_vote.csv'
 file_current = os.path.join(folder_current, folder_csv, file_csv)
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=os.path.join(
+    os.getcwd(), 
+    os.getenv('JSON_NAME')
+)
+schema = 'Sex:string, Age:string, Year:string, Month:string, Day:string,'\
+        'Street_Number:string, Birthday:string,Name:string, Surname:string,'\
+        'City:string, Street:string, State:string, State_Voted:string'
+
+project_id = os.getenv('PROJECT_ID')
+dataset = os.getenv('DATASET')
+table_id = os.getenv('TABLE_ID')
+
+temp_location = os.getenv("TEMP_LOCATION")
+staging_location=os.getenv("STAGING_LOCATION")
+output=os.getenv('OUTPUT')
